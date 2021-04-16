f2f161dce882a3755bb648d8c94651995e472e6b
Jake Williams <jbwilliams84@gmail.com>
2021-04-16 07:25:50 -0700

    working on js

--- script.js
+++ script.js
@@ -5,5 +5,56 @@ var cityList = JSON.parse(localStorage.getItem("city-list"));
 if (!cityList) {
     cityList = [];
 }
+console.log("city-list")
 console.log(cityList); 
+
+// UV condition function 
+function uvCondition(value) {
+    if (value < 2.5) {
+        return "uv-low";
+    }
+    else if (value < 5.5) {
+        return "uv-moderate";
+    }
+    else if (value < 7.5) {
+        return "uv-high";
+    }
+    else if (value <10.5) {
+        return "uv-vhigh";
+    }
+    else {
+        return "uv-extreme";
+    }
+}
+
+// Search history function
+function renderSearchHistory() {
+    // Clears search history
+    $("#search-history").empty();
+    // Cycles through city list
+    $.each(cityList, function(index, city) {
+        // Creates a list item
+        var liEl = $('<li class="search-item">').text(city);
+        $("search-history").append(liEl);
+    });
+}
+
+// Function updating search history
+function updateSearchHistory(cityName) {
+    // Removes city from list if already there
+    if (cityList.includes(cityName)) {
+        cityList.splice(cityList.indexOf(cityName), 1);
+    }
+// Adds city to top of list
+var listLength = cityList.unshift(cityName);
+/* Only allows for 8 cities in list, will remove the last one if more added */
+if (listLength >8) {
+     cityList.pop();
+}
+// Stores the list in local storage
+localStorage.setItem("city-list", JSON.stringify(cityList));
+
+// Displays search history
+renderSearchHistory();
+}
 
